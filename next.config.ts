c138dcfd470ac0447cd16f3d05232aa4e68aafcd
Christopher KADE <c.kade96@gmail.com>
2026-03-05 11:46:54 +0100

    Cleaned up config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/grand-arbiter',
-  assetPrefix: '/grand-arbiter',
-  images: {
-    unoptimized: true
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
